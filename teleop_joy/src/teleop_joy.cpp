7657d2db13b3b9ce46e8afe4a27a084774fef77e
Barham Farraj <77174040+Farraj007@users.noreply.github.com>
2025-02-24 14:47:52 +0100

    Updated way to kill ros2 by button from controller

--- teleop_joy/src/teleop_joy.cpp
+++ teleop_joy/src/teleop_joy.cpp
@@ -59,7 +59,8 @@ private:
         {
             subscribe_to_teleop();
             RCLCPP_WARN(this->get_logger(), "Kill switch activated, shutting down...");
-            std::system("killall -9 ros2");
+            //std::system("killall -9 ros2");
+            std:system("pkill -f '/opt/ros/foxy'"
         }
     }
     void subscribe_to_drive()
